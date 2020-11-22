6fa48f71cf8b1de4c2ad75aaaddd1393d69cbc56
unknown <kaoutharben1998@gmail.com>
2020-11-22 20:29:12 +0100

    commito

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -8,9 +8,7 @@ class InsertionSort implements Strategy {
             int index = liste.get(i);
             int j = i - 1;
 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
+
             while (j >= 0 && liste.get(j) > index) {
                 liste.set(j + 1, liste.get(j));
                 j = j - 1;
